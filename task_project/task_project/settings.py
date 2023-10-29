b639c81bd10f42309a50e336662cf4ad5f5e9ea6
reinikeda <reinikeda@gmail.com>
2023-10-29 12:56:08 +0200

    small fix

--- task_project/task_project/settings.py
+++ task_project/task_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from . import local_settings
+# from . import local_settings
 import django_heroku
 import os
 import dj_database_url
